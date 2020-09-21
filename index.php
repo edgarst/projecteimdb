a431d2ae5e90b03f5afd14eeae4ca4a0fa76f7ca
LAPTOP-VDQSU64P\esegui <esegui@gnahs.com>
2020-09-21 11:40:04 +0200

    Added includes

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
-<link rel="stylesheet" href="css/style.css"> 
+<link rel="stylesheet" href="css/style.css">
+
 <?php $version = "v2.0.0" ?>
+
+<?php require("./include/connection_db.php"); ?>
 <?php include('content/header.php') ?>
 <?php include('content/content.php') ?>
