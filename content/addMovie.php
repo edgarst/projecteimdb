caccd297e34f3dc0494f60f8283b402449854394
LAPTOP-VDQSU64P\esegui <esegui@gnahs.com>
2020-09-21 11:41:11 +0200

    Add check required form fields

--- content/addMovie.php
+++ content/addMovie.php
@@ -1,8 +1,10 @@
 <?php include("header.php"); ?>
-<?php require_once("../include/connection_db.php"); ?>
+<?php include("../include/image.php"); ?>
+<?php require("../include/connection_db.php"); ?>
 <link rel="stylesheet" href="../css/style.css"> 
 
 <?php 
+    // Stream platform query
     try{
         $sql = $connect->prepare('SELECT * FROM plataforma');
         $sql->execute(array());
@@ -16,13 +18,60 @@
     }catch(PDOException $e){
         echo "ERROR: " . $e->getMessage();
     }
+
+    // Check for errors
+    $title_error = $sinopsis_error = $img_error = "";
+    function empty_title($title){
+        global $title_error;
+        if (empty($title)) {
+            $title_error = "Title is required";
+            return true;
+        }
+        return false;
+    }
+
+    function empty_sinopsis($sinopsis){
+        global $sinopsis_error;
+        if (empty($sinopsis)) {
+            $sinopsis_error = "Sinopsis is required";
+            return true;
+        }
+        return false;
+    }
+
+    function empty_image($img){
+        global $img_error;
+        if (empty($img)) {
+            $img_error = "Image is required";
+            return true;
+        }
+        return false;
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Check Required files
+        $title = $_POST["title"];
+        $title_ok = !empty_title($title);
+        
+        $sinopsis = $_POST["sinopsis"];
+        $sinopsis_ok = !empty_sinopsis($sinopsis);
+
+        $img = $_POST["caratula"];
+        $img_ok = !empty_image($img);
+
+        if ($title_ok && $sinopsis_ok && $img_ok) {
+            $image = new Image();
+            $img_error = $image->check_image($_FILES);
+        }
+    }
 ?>
 
 <section class="form-movie">
     <h4>Afegir nova pel·lícula</h4>
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" enctype="multipart/form-data">
-        <input class="movie-data" type="text" name="title" placeholder="Títol">
         <span class="error">* <?php echo $title_error; ?></span>
+        <input class="movie-data" type="text" name="title" placeholder="Títol">
+        <span class="error">* <?php echo $sinopsis_error; ?></span>
         <textarea class="movie-data" name="sinopsis" cols="30" rows="5" placeholder="Sinopsis"></textarea>
         <p>Publicació:</p>
         <input class="movie-data" type="date" name="published">
@@ -32,50 +81,11 @@
                 <option value="<?php echo $plataforma[$i] ?>"> <?php echo $plataforma[$i] ?></option>
             <?php } ?>
         </select>
-        <p>Caràtula:</p>
+        <span class="error">* <?php echo $img_error; ?></span>
+        <p id="caratula-text">Caràtula:</p>
         <input class="movie-data" type="file" name="caratula">
+        <span class="error">* Required fields</span>
         <button type="submit">Enviar</button>
     </form>
 </section>
 
-<?php
-    $title_error = $img_error = "";
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Comprovació imatge
-        $img_folder = "./content/img/";
-        $file = $img_folder . basename($_FILES["caratula"]["name"]);
-        $uploadOk = 1;
-        $imageFileType = strtolower(pathinfo($file,PATHINFO_EXTENSION));
-    
-        // Comprovar que és una imatge
-        if(isset($_POST["submit"])) {
-            $check = getimagesize($_FILES["caratula"]["tmp_name"]);
-            if($check == false) {
-                $img_error = "El fitxer no és una imatge";
-                $uploadOk = 0;
-            }
-        }
-        // Comprovar si el fitxer ja existeix
-        if (file_exists($file)) {
-            $img_error = "El fitxer ja existeix.";
-            $uploadOk = 0;
-        }
-    
-        // Limitació del tamany
-        if ($_FILES["caratula"]["size"] > 500000) {
-            $img_error = "Ho sento el fitxer pesa massa";
-            $uploadOk = 0;
-        }
-    
-        if ($uploadOk == 0) {
-            $img_error = "El fitxer no s'ha penjat. " . $img_error;
-            // Si és OK pujarà l'arxiu
-        } else {
-            if (move_uploaded_file($_FILES["caratula"]["tmp_name"], $file)) {
-                $img_error = "El fitxer ". basename( $_FILES["caratula"]["name"]). " s'ha penjat correctament.";
-            } else {
-                $img_error = "Hi ha hagut un error a l'hora de penjar el fitxer. Torna a provar en un altre moment.";
-            }
-        }
-    }
-?>
