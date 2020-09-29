76ed4d85f35d92a12dd9ce721d48566920208990
LAPTOP-VDQSU64P\esegui <esegui@gnahs.com>
2020-09-29 09:26:08 +0200

    Updated Required Form Fields

--- src/content/FormMovie.php
+++ src/content/FormMovie.php
@@ -10,28 +10,26 @@ include("header.html");
 <link rel="stylesheet" href="../css/style.css"> 
 
 
-<?php 
-    
-?>
-
 <section class="form-movie">
     <h4>Afegir nova pel·lícula</h4>
     <form method="post" action="" enctype="multipart/form-data">
-        <span class="error">* <?php echo $title_error; ?></span>
+        <span class="error">*</span>
         <input class="movie-data" type="text" name="title" placeholder="Títol" required>
-        <span class="error">* <?php echo $sinopsis_error; ?></span>
-        <textarea class="movie-data" name="sinopsis" cols="30" rows="5" placeholder="Sinopsis"></textarea>
-        <p>Publicació:</p>
-        <input class="movie-data" type="date" name="published">
+        <span class="error">*</span>
+        <textarea class="movie-data" name="sinopsis" cols="30" rows="5" placeholder="Sinopsis" required></textarea>
+        <span class="error">*</span>
+        <p id="published-date">Publicació:</p>
+        <input class="movie-data" type="date" name="published" required>
         <input class="movie-data" type="number" name="valoracio" placeholder="Valoració">
-        <select class="movie-data" name="plataforma">
+        <span class="error">*</span>
+        <select class="movie-data" name="plataforma" required>
             <?php for ($i=0; $i < count($plataforma); $i++) { ?>
                 <option value="<?php echo $plataforma[$i] ?>"> <?php echo $plataforma[$i] ?></option>
             <?php } ?>
         </select>
-        <span class="error">* <?php echo $img_error; ?></span>
+        <span class="error">*</span>
         <p id="caratula-text">Caràtula:</p>
-        <input class="movie-data" type="file" name="caratula">
+        <input class="movie-data" type="file" name="caratula" required>
         <span class="error">* Required fields</span>
         <button type="submit">Enviar</button>
     </form>
