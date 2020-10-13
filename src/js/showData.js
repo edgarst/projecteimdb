ecb79c71e6868967da7d496eb7f0b76c4c56482e
LAPTOP-VDQSU64P\esegui <esegui@gnahs.com>
2020-10-13 10:28:06 +0200

    JavaScript: Show info in html (completed)

--- src/js/showData.js
+++ src/js/showData.js
@@ -1,7 +1,10 @@
+document.getElementById('search-btn').addEventListener('click', showMovies);
+
 function showMovies()
 {
     var title = document.getElementById('search-input').value;
-    var url = 'http://imdbcutre.test/informacio.php?title='+title;
+    var url = 'http://imdbcutre.test/informacio.php?search='+title;
+
     fetch(url).then(function(response){ return response.json(); })
     .then(function(json){ viewFilms(json); });
 }
@@ -14,31 +17,73 @@ function viewFilms(json)
 
 function setFilmText(json)
 {
-    var text;
-    var i = 1;
-    for (const value of json) {
-        console.log(value);
-        text = getFilmData(value);
-        document.getElementById('film'+i).style.display = 'block';
-        document.getElementById('title'+i).innerHTML = value['titol'];
-        document.getElementById("img"+i).src = value['caratula'];
-        i++;
+    var row = createRow();
+    for (const film of json) {
+        var col = createCol();
+        var img = createImg(film['caratula']);
+        var divText = createDivTitle();
+        var text = createTitle(film['titol']);
+        var link = createTitleLink(film['titol']);
+        appendFilm(link, text, divText, col, img, row);
     }
 }
 
-function getFilmData(json)
+function createRow()
 {
-    var text = '';
-    for (const key in json) {
-        if(key != 'titol' && key != 'caratula') text += key+': '+json[key]+', '; 
-    }
+    var row = document.createElement('div');
+    row.setAttribute('class', 'row');
+    return row
+}
+
+function createCol()
+{
+    var col = document.createElement('div');
+    col.setAttribute('class', 'col');
+    return col;
+}
+
+function createImg(linkImg) 
+{
+    var img = document.createElement('img');
+    img.setAttribute('alt', 'error');
+    img.setAttribute('src', linkImg);
+    return img;
+}
+
+function createDivTitle()
+{
+    var divText = document.createElement('div');
+    divText.setAttribute('class', 'text-img');
+    return divText;
+}
+
+function createTitle(title)
+{
+    var text = document.createElement('h4');
+    text.innerHTML = title;
     return text;
+}
+
+function createTitleLink(title)
+{
+    var link = document.createElement('a');
+    link.setAttribute('href', 'http://imdbcutre.test/informacio.php?name='+title);
+    return link;
+}
+
+function appendFilm(link, text, divText, col, img, row) {
+    link.appendChild(text);
+    divText.appendChild(link);
+    col.appendChild(img);
+    col.appendChild(divText);
+    row.appendChild(col);
+    document.body.appendChild(row);
 }
 
 function resetDivs()
 {
-    var divs = document.getElementsByClassName('col');
+    var divs = document.getElementsByClassName('row');
     for (var i = 0; i < divs.length; i++) {
-        divs[i].innerHTML = '';
+        document.body.removeChild(divs[i]);
     }
 }
