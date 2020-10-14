fbd2f09519b1dbb1f55030ba46873e804a320894
LAPTOP-VDQSU64P\esegui <esegui@gnahs.com>
2020-10-14 11:54:25 +0200

    Add Event Listener to search button

--- src/js/home.js
+++ src/js/home.js
@@ -3,4 +3,6 @@ window.onload = function()
     var url = 'http://imdbcutre.test/information.php?home';
     fetch(url).then(function(response){ return response.json(); })
     .then(function(json){ viewFilms(json); });
+    
+    document.getElementById('search-btn').addEventListener('click', showMovies);
 }
