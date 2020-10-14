1e11e1614c42f2fe07fa758bf9b0ade11985d1b4
LAPTOP-VDQSU64P\esegui <esegui@gnahs.com>
2020-10-14 11:07:19 +0200

    Add JavaScript to FormMovie (completed): Choose between 1/2 directors, create inputs and show Platforms Name

--- src/js/form.js
+++ src/js/form.js
@@ -3,13 +3,14 @@ window.onload = function()
     var url = 'http://imdbcutre.test/information.php?platforms';
     fetch(url).then(function(response){ return response.json(); })
     .then(function(json){ setPlatforms(json); });
+    
+    document.getElementById('director1').addEventListener('change', checkRadioInput);
+    document.getElementById('director2').addEventListener('change', checkRadioInput);
 }
 
 function setPlatforms(json)
 {
-    console.log(json);
     for (const platform of json) {
-        console.log(platform);
         var option = createPlatformOption(platform['nom']);
         appendOption(option);
     }
@@ -18,7 +19,7 @@ function setPlatforms(json)
 function createPlatformOption(platformName)
 {
     var option = document.createElement('option');
-    option.setAttribute('value', platformName);
+    option.value = platformName;
     option.innerHTML = platformName;
     return option;
 }
@@ -29,21 +30,27 @@ function appendOption(option)
     select.appendChild(option);
 }
 
-document.getElementById('director1').addEventListener('onchange', checkRadioInput);
-document.getElementById('director2').addEventListener('onchange', checkRadioInput);
 function checkRadioInput()
 {
     var selected = document.querySelector('input[name="director"]:checked').value;
-    selected=1 ? createDirectorInputs(1) : createDirectorInputs(2);
+    deleteClass('directors');
+    createDirectorInputs(selected);
 }
 
 function createDirectorInputs(num)
 {
-    var placeAppend = document.getElementById('rating');
+    var placeAppend = document.getElementById('info-btn');
     for (let index = 0; index < num; index++) {
-        var input = document.createElement('input');
-        input.setAttribute('class', 'movie-data');
-        input.setAttribute('type', 'text');
+        var input = newDirectorInput(index);
         placeAppend.parentNode.insertBefore(input, placeAppend);
     }
+}
+
+function newDirectorInput(index)
+{
+    var input = document.createElement('input');
+    input.className = 'movie-data directors';
+    input.type = 'text';
+    input.placeholder = 'Director '+ ++index;
+    return input;
 }
