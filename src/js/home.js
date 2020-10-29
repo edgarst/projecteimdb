ae077f8d13cab6e3580b56bc3106a8854c14fbd5
LAPTOP-VDQSU64P\esegui <esegui@gnahs.com>
2020-10-29 09:43:49 +0100

    Revert "Delete function, is already in other file"
    This reverts commit 252d80460a4ce18c65cf45427404462e96940ea3.

--- src/js/home.js
+++ src/js/home.js
@@ -3,4 +3,15 @@ window.onload = function()
     var url = 'http://imdbcutre.test/information.php?home';
     fetch(url).then(function(response){ return response.json(); })
     .then(function(json){ viewFilms(json); });
+    
+    document.getElementById('search-btn').addEventListener('click', showMovies);
+}
+
+function showMovies()
+{
+    var title = document.getElementById('search-input').value;
+    var url = 'http://imdbcutre.test/information.php?search='+title;
+
+    fetch(url).then(function(response){ return response.json(); })
+    .then(function(json){ viewFilms(json); });
 }
